17933cb8a2bc9bc2f5cb6deaa465094f9b0b593f
jec9386 <jerrychandev@gmail.com>
2025-04-23 08:12:00 -0400

    checkpoint- book is done

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -7,12 +7,12 @@ public class Book {
     private boolean isCheckedOut;
     private String checkedOutTo;
 
-    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+    public Book(int id, String isbn, String title) {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
-        this.isCheckedOut = isCheckedOut;
-        this.checkedOutTo = checkedOutTo;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
     }
 
     public int getId() {
@@ -43,20 +43,14 @@ public class Book {
         return isCheckedOut;
     }
 
-    public void setCheckedOut(boolean checkedOut) {
-        isCheckedOut = checkedOut;
-    }
 
     public String getCheckedOutTo() {
         return checkedOutTo;
     }
 
-    public void setCheckedOutTo(String checkedOutTo) {
-        this.checkedOutTo = checkedOutTo;
-    }
 
 
-    //create methods, how the book should reflect these actions
+    //create methods, how the book should reflect these actions, can change data through one og these 2 methods.
     public void checkOut(String name){
         this.isCheckedOut = true;
         this.checkedOutTo = name;
